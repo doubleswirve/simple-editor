45ae36ed6f27207762daec9a606daa5a15e400f7
Saxon Stiller <doubleswirve@gmail.com>
2013-11-10 17:48:00 -0600

    clean up some globals and prevent first child element from being deleted

--- js/directives/contenteditable.js
+++ js/directives/contenteditable.js
@@ -10,6 +10,12 @@ editorApp.directive('contenteditable', [function(){
       if (!ngModel) return;
 
       /**
+       * Grab the first child so it cannot be deleted
+       */
+
+      var firstChild;
+
+      /**
        * ngModel render function
        */
 
@@ -18,11 +24,10 @@ editorApp.directive('contenteditable', [function(){
         val = val
           .replace(/&nbsp;/g, ' ')
           .replace(/\s{2,}/g, ' ')
-          .replace(/\s*(<\/?p>)\s*/g, function(match, p1){
-            return p1;
-          })
           .trim();
         el.html(val);
+
+        firstChild = el[0].firstChild;
       };
 
       /**
@@ -52,6 +57,7 @@ editorApp.directive('contenteditable', [function(){
       /**
        * Selection object for cursor position/highlighting
        */
+
       var userSelection = window.getSelection()
         , range
         , preCaretRange;
@@ -64,6 +70,8 @@ editorApp.directive('contenteditable', [function(){
         ];
 
       var focusNode, focusOffset;
+
+      var keyCode;
 
       function setCursor(parentNode, parentOffset) {
         var charIndex = 0, nextCharIndex, len, delta;
@@ -100,13 +108,73 @@ editorApp.directive('contenteditable', [function(){
        * Detect certain keyboard events
        */
 
-      el.bind('keypress', function(evt){
+      el.bind('keydown', function(evt){
+
+        keyCode = evt.which;
+
+        // TODO: maybe do an array check like below
+        if (
+          keyCode !== 13 &&
+          keyCode !== 8 &&
+          keyCode !== 32
+        ) {
+          return true;
+        }
 
         /**
          * Ignore carriage returns if specified by the directive attribute
          */
 
-        if (ignoreReturn && 13 === evt.which) {
+        if (ignoreReturn && 13 === keyCode) {
+          return evt.preventDefault();
+        }
+
+        /**
+         * Obtain the focus node and the caret's relative offset
+         */
+
+        focusNode = userSelection.focusNode;
+        focusOffset = userSelection.focusOffset;
+
+        /**
+         * Obtain an eligible parent node by traversing up while indexOf is -1
+         */
+
+        if (3 !== userSelection.anchorNode.nodeType) {
+          console.log('here');
+          parentNode = focusNode;
+        } else{
+          parentNode = userSelection.anchorNode.parentNode;
+
+          while (!~eligibleParentNodes.indexOf(parentNode.tagName.toUpperCase())) {
+            parentNode = parentNode.parentNode;
+          }
+        }
+
+        parentTextContent = parentNode.textContent;
+
+        /**
+         * Obtain selection range to determine offset relative to parent
+         */
+
+        range = userSelection.getRangeAt(0);
+        preCaretRange = range.cloneRange();
+        preCaretRange.selectNodeContents(parentNode);
+        preCaretRange.setEnd(range.endContainer, range.endOffset);
+
+        parentOffset = preCaretRange.toString().length;
+
+        /**
+         * Prevent backspace from deleting the first child node (e.g., We don't want
+         * to delete the first P tag, because I don't think we're planning on having
+         * a P tag button in the tooltip)
+         */
+
+        if (
+          8 === keyCode &&
+          !parentOffset &&
+          parentNode === firstChild
+        ) {
           return evt.preventDefault();
         }
 
@@ -114,42 +182,7 @@ editorApp.directive('contenteditable', [function(){
          * Emulate Medium's single whitespace policy
          */
 
-        if (32 === evt.which) {
-
-          offset = userSelection.focusOffset;
-          text = userSelection.anchorNode.data;
-          html = userSelection.anchorNode.parentNode.innerHTML;
-
-          /**
-           * Obtain the focus node and the caret's relative offset
-           */
-
-          focusNode = userSelection.focusNode;
-          focusOffset = userSelection.focusOffset;
-
-          /**
-           * Obtain an eligible parent node by traversing up while indexOf is -1
-           */
-
-          parentNode = userSelection.anchorNode.parentNode;
-
-          while (!~eligibleParentNodes.indexOf(parentNode.tagName.toUpperCase())) {
-            parentNode = parentNode.parentNode;
-          }
-
-          parentTextContent = parentNode.textContent;
-
-          /**
-           * Obtain selection range to determine offset relative to parent
-           */
-
-          range = userSelection.getRangeAt(0);
-          preCaretRange = range.cloneRange();
-          preCaretRange.selectNodeContents(parentNode);
-          preCaretRange.setEnd(range.endContainer, range.endOffset);
-
-          parentOffset = preCaretRange.toString().length;
-
+        if (32 === keyCode) {
 
           /**
            * The caret focus was preceded by whitespace or at beginning of parent
@@ -166,13 +199,16 @@ editorApp.directive('contenteditable', [function(){
            * The caret focus is right before a whitespace
            */
 
-          if (/\s+/g.test(parentTextContent[parentOffset])) {
+          if (
+            /\s+/g.test(parentTextContent[parentOffset]) &&
+            parentOffset + 1 < parentTextContent.length
+          ) {
             evt.preventDefault();
 
             /**
              * Still move the caret one space ahead
              */
-
+            
             setCursor(parentNode, parentOffset + 1);
           }
         }
