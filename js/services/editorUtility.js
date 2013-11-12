708ead3e5262d2df4d988b6c5be4de0a890f511f
Saxon Stiller <doubleswirve@gmail.com>
2013-11-12 00:39:30 -0600

    add editor service for some shared methods

---
+++ js/services/editorUtility.js
@@ -0,0 +1,36 @@
+/**
+ * Service providing DOM-based helper functions 
+ * that are used across directives
+ */
+
+editorApp.service('editorUtility', [function(){
+
+  /**
+   * Eligible container nodes. This is a constraint 
+   * array to determine offset point for ranges. The 
+   * goal is to keep the markup clean and the structure 
+   * manageable.
+   */
+
+  this.eligibleContainerNodes = ['H1', 'H2', 'P', 'LI', 'UL'];
+
+  /**
+   * Retrieve an eligible parent node relative to the
+   * selection 
+   */
+
+  this.getContainerNode = function(selection){
+    var anchorNode = selection.anchorNode
+      , parentNode = anchorNode.parentNode;
+
+    if (3 !== anchorNode.nodeType) return selection.focusNode;
+
+    while (
+      !~this.eligibleContainerNodes.indexOf(parentNode.tagName.toUpperCase())
+    ) {
+      parentNode = parentNode.parentNode;
+    }
+
+    return parentNode;
+  };
+}]);
